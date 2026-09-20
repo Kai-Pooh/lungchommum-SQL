d4b24b800cddcbe5ec3d2ee7dd30c8847c588613
Npka01 <94159629+Kai-Pooh@users.noreply.github.com>
2026-09-20 23:31:04 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { supabase } from "../../lib/supabaseClient";
+import { supabase } from "../lib/supabaseClient";
 
 // ตั้งเกณฑ์แจ้งเตือนสต๊อกใกล้หมด
 const LOW_STOCK_THRESHOLD = 5;
